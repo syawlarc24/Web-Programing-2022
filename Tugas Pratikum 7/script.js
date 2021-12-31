39cff3ca14d2ea3be7154b8f505013fd4cd57162
syawlarc24 <93024504+syawlarc24@users.noreply.github.com>
2022-01-01 01:27:37 +0700

    Add files via upload

---
+++ Tugas Pratikum 7/script.js
@@ -0,0 +1,13 @@
+console.log('Hello, Syawla!')
+
+// Belajar Variabel
+// statis
+const phi = 3.14;
+// phi = 3.15; error tidak bisa dikasih nilai ulang 
+console.log (phi)
+
+
+// dinamis
+let age = 19;
+age = 20;
+console.log (age)
